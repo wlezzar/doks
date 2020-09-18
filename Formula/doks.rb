afda5ea7f5db860e58e4044243f0cfd3dea8ac9d
Walid Lezzar <walid.lezzar@schibsted.com>
2020-09-18 08:08:22 +0200

    bump formula to 0.5.0

--- Formula/doks.rb
+++ Formula/doks.rb
@@ -2,10 +2,10 @@ class Doks < Formula
   desc "Provides a simple search engine over your distributed documentation"
   homepage "https://github.com/wlezzar/doks"
   bottle :unneeded
-  version "0.4.0"
+  version "0.5.0"
   
-  url "https://github.com/wlezzar/doks/releases/download/0.4.0/doks.zip"
-  sha256 "b7dc9629bba7fd952ba9af9f830a19efd53cabc415e55456710680c48d14bdbe"
+  url "https://github.com/wlezzar/doks/releases/download/0.5.0/doks.zip"
+  sha256 "4d42042a549eea586ac6d56f9e2dfd564d3e76366879fe477fb86429407466e6"
 
   def install
     bin.install "bin/doks"
