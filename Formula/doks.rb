e5f741b2e07d2919632e84bcc47bc9260d55a278
wlezzar <wlezzar@users.noreply.github.com>
2020-09-28 11:31:26 +0000

    [auto] Update brew formula

--- Formula/doks.rb
+++ Formula/doks.rb
@@ -2,10 +2,10 @@ class Doks < Formula
   desc "Provides a simple search engine over your distributed documentation"
   homepage "https://github.com/wlezzar/doks"
   bottle :unneeded
-  version "0.9.0"
+  version "0.9.1"
   
-  url "https://github.com/wlezzar/doks/releases/download/0.9.0/doks.zip"
-  sha256 "af07f7446acc514a3725b28133c113953b64b023328a41bf2efec1f14c3ce336"
+  url "https://github.com/wlezzar/doks/releases/download/0.9.1/doks.zip"
+  sha256 "4be81801e68adaf7dfff28d33f1608d56c96beec8d8975299c1b5c7f8c3149ce"
 
   def install
     bin.install "bin/doks"
